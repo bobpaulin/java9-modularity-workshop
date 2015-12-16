3ea61ad237ce54788c836e836ff6808afaf459a8
bpaulin <bpaulin@DESKTOP-2K8DFK0>
2015-12-16 08:00:44 -0600

    Example 1

---
+++ example-1/src/com/bobpaulin/modularity/mod1/Mod1.java
@@ -0,0 +1,13 @@
+package com.bobpaulin.modularity.mod1;
+
+public class Mod1 {
+    public static void main(String[] args) {
+        Mod1 module1 = new Mod1();
+        module1.doSomthing();
+    }
+    
+    public void doSomthing()
+    {
+        System.out.println("Mod1");
+    }
+}
